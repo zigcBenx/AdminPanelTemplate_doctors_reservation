ac2668c8876c1dcea322f9da61b1f478f85fb2dd
zigcBenx <zigac.benko@gmail.com>
2020-06-06 12:28:16 +0200

    Updating perscriptions stuff

--- resources/views/admin/recepti.blade.php
+++ resources/views/admin/recepti.blade.php
@@ -14,7 +14,7 @@
 
                 <select name="" id="users-doctors-list" class="form-control float-right" >
                 </select>
-                <a href="/admin" class="btn btn-success">Zahtevaj recept <i class="fas fa-receipt"></i></a>
+                <a href="javascript:;" class="btn btn-success" id="request-receipt">Zahtevaj recept <i class="fas fa-receipt"></i></a>
             </div>
             <!-- /.input group -->
         </div>
@@ -32,9 +32,94 @@
             <p>Sasd asff dsfd sdf.</p>
         </div>
     </div>
-
+@include('partials.receipt-modal')
 @endsection
 @section('scripts')
     @parent
+
+    <script>
+        $(document).ready(function() {
+            /**
+             * Show modal
+             */
+            $('#request-receipt').click(function(){
+                if(!$.isNumeric($("#users-doctors-list").val())){
+                    alert("Prosimo izberite zdravnika.");
+                    return;
+                }
+                $('#doctor_name').html($("#users-doctors-list option:selected").text());
+                $('#receipt-modal').modal('show');
+
+            });
+
+            /**
+             * Submit receipt request
+             */
+            $('#request-receipt-button').click(function(){
+                $.post('{{route('admin.get-work-place')}}', {_token: "{{ csrf_token() }}", docId:$("#users-doctors-list").val()})
+                    .done(function(data) {
+                        let workplaceOfselectedDoctor = data[0].workspace;
+                        let dataToSubmit = {
+                            "WorkplaceCode": workplaceOfselectedDoctor,
+                            "DoctorIVZCode": $("#users-doctors-list").val(),
+                            "RequestId": $.now(),
+                            "Patient": {
+                                "KzzNumber": $('#user_kzz').val(),
+                                "Email": $('#user_mail').val(),
+                                "Phone": $('#user_phone').val()
+                            },
+                            "Comment": $('#pripomba').val(),
+                            // "Attachments": [
+                            //     {
+                            //         "Name": "string",
+                            //         "Content": "string",
+                            //         "Comment": "string"
+                            //     }
+                            // ],
+                            "ProviderZZZSNumber": "102320",
+                            "Client": {
+                                "UniqueDeviceId": "A3DE534DB",
+                                "ClientType": "browser (User-Agent)",
+                                "ApplicationVersion": "1.22",
+                                "ApplicationId": "myXlife"
+                            }
+                        };
+                        $.post("https://enarocanje-gw1.comtrade.com/ctNarocanjeTest/api/ElektronskoNarocanje/RequestPrescription", dataToSubmit)
+                            .done(function (data) {
+                                if(!data.IsSuccessful){
+                                    alert(data.ErrorDescription);
+                                    // return;
+                                }
+                                console.log(data);
+                                // do whač ju goda do
+                            });
+                    });
+            });
+
+            $.post('{{ route("admin.user-doctor-show") }}', {_token: "{{ csrf_token() }}"})
+                .done(function (data) {
+                    $('#users-doctors-list').html('');
+                    if (data.length == 0) {
+                        $('.loading').hide();
+                        $("#no-doctor").show();
+                        return;
+                    }
+                    displayDoctor = data[0].doctor_id;
+                    $("#users-doctors-list").append('<option>Izberi zdravnika</option>')
+                    for (let i = 0; i < data.length; i++) {
+                        let doctorId = data[i].doctor_id;
+                        fetch('https://enarocanje-gw1.comtrade.com/ctNarocanjeTest/api/ElektronskoNarocanje/GetDoctorInfo?request.doctorIVZCode=' + doctorId + '&request.providerZZZSNumber=102320&request.client.uniqueDeviceId=A3DE534DB&request.client.clientType= browser (User-Agent): Mozilla/5.0&request.client.applicationVersion=1.22&request.client.applicationId=myXlife')
+                            .then(res => res.json())
+                            .then(res => {
+                                let doctorsFirstName = res.DoctorInfos[0].DoctorFirstName;
+                                let doctorsLastName = res.DoctorInfos[0].DoctorLastName;
+                                $("#users-doctors-list").append('<option value="' + res.DoctorInfos[0].DoctorIVZCode + '">' + doctorsFirstName + ' ' + doctorsLastName + '</option>')
+                            });
+                    }
+                    $('.loading').hide();
+
+                });
+        });
+    </script>
 
 @endsection
---
+++ resources/views/partials/receipt-modal.blade.php
@@ -0,0 +1,31 @@
+<div class="modal fade" id="receipt-modal">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h4 class="modal-title">Rezervacija termina</h4>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">×</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <div class="form-group">
+                    <b>Zdravnik: </b> <span id="doctor_name"></span>
+                </div>
+                <div class="form-group">
+                    <label for="pripomba">Pripombe:</label>
+                    <textarea class="form-control" name="" id="pripomba" cols="8" rows="1" placeholder="Tukaj vnesite morebitne pripombe ..."></textarea>
+                    {{--                    <i>Priloge</i>--}}
+                </div>
+            </div>
+            <input type="hidden" id="user_kzz" value="{{$user->zzzs}}">
+            <input type="hidden" id="user_phone" value="{{$user->phone}}">
+            <input type="hidden" id="user_mail" value="{{$user->email}}">
+            <div class="modal-footer justify-content-between">
+                <button type="button" class="btn btn-warning" data-dismiss="modal">Zapri</button>
+                <button type="button" class="btn btn-success" id="request-receipt-button">Pošlji</button>
+            </div>
+        </div>
+        <!-- /.modal-content -->
+    </div>
+    <!-- /.modal-dialog -->
+</div>
