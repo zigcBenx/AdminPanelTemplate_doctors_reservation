ad7eda9edb7a98f9ac278f0f3357fe86daf4b171
zigcBenx <zigac.benko@gmail.com>
2020-06-15 20:53:48 +0200

    asd

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -32,7 +32,7 @@ class HomeController
         $endpoint = "https://durs.comtrade.com/ctNarocanje/api/ElektronskoNarocanje/GetDoctors?request.providerZZZSNumber=102320&request.client.uniqueDeviceId=A3DE534DB&request.client.clientType=Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:75.0) Gecko/20100101 Firefox/75.0&request.client.applicationVersion=1.22&request.client.applicationId=myXlife";
         $client = new \GuzzleHttp\Client();
 
-        $response = $client->request('GET', $endpoint);
+        $response = $client->request('GET', $endpoint,['verify' => false]);
 
 // url will be: http://my.domain.com/test.php?key1=5&key2=ABC;
 
