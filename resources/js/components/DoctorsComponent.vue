ae6742aee7bfdabb0ca41e2c38b209364d68e128
zigcBenx <zigac.benko@gmail.com>
2020-06-15 20:49:49 +0200

    asd

--- resources/js/components/DoctorsComponent.vue
+++ resources/js/components/DoctorsComponent.vue
@@ -53,7 +53,7 @@
     	},
     	methods: {
     		fetchDoctors(){
-    			fetch('https://durs.comtrade.com/ctNarocanje/api/ElektronskoNarocanje/GetDoctors?request.providerZZZSNumber=102320&request.client.uniqueDeviceId=A3DE534DB&request.client.clientType=Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:75.0) Gecko/20100101 Firefox/75.0&request.client.applicationVersion=1.22&request.client.applicationId=myXlife',{mode:'cors'})
+    			fetch('https://durs.comtrade.com/ctNarocanje/api/ElektronskoNarocanje/GetDoctors?request.providerZZZSNumber=102320&request.client.uniqueDeviceId=A3DE534DB&request.client.clientType=Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:75.0) Gecko/20100101 Firefox/75.0&request.client.applicationVersion=1.22&request.client.applicationId=myXlife')
     			.then( res => res.json())
     			.then( res => {
     			    console.log("SUCCESS:");
