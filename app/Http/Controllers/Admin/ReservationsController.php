25495db598879ebda7d93570daf2b2edd5554bf9
zigcBenx <zigac.benko@gmail.com>
2020-07-15 07:11:10 +0200

    fixed reseravtaions db

--- app/Http/Controllers/Admin/ReservationsController.php
+++ app/Http/Controllers/Admin/ReservationsController.php
@@ -5,16 +5,17 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Reservations;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ReservationsController extends Controller
 {
     public function createReservation(Request $request){
         $reservation = new Reservations();
-        $reservation->slotId = $request->get('params["Slot"]["SlotId"]');
-        $reservation->user_id = 1;
-        $reservation->workplace_id = 2;
-        $reservation->start = $request->get('params["Slot"]["Start"]');
-        $reservation->end = $request->get('params["Slot"]["End"]');
+        $reservation->slotId = $request->get('params.Slot.SlotId');
+        $reservation->user_id = Auth::id();
+        $reservation->workplace_id = $request->get('params.WorkplaceCode');
+        $reservation->start = $request->get('params.Slot.Start');
+        $reservation->end = $request->get('params.Slot.End');
         $reservation->canceled = false;
 
         $reservation->save();
